fc1f88545727497c8667766eda33b3857252de97
rbasamoyai <b33gy0sh@gmail.com>
2023-09-16 16:04:18 -0400

    ♻ Tweak pith helmet UV and texture

--- common/src/main/java/rbasamoyai/suitableforcombat/content/items/hats/PithHelmetModel.java
+++ common/src/main/java/rbasamoyai/suitableforcombat/content/items/hats/PithHelmetModel.java
@@ -20,9 +20,8 @@ public class PithHelmetModel extends BasicHatModel {
 		PartDefinition part = mesh.getRoot();
 		PartDefinition head = part.getChild("head");
 		head.addOrReplaceChild("helmet", CubeListBuilder.create()
-				.texOffs(32, 44).addBox(-4.0F, -4.0F, -4.0F, 8.0F, 5.0F, 8.0F, CubeDeformation.NONE)
+				.texOffs(32, 44).addBox(-4.0F, -4.0F, -4.0F, 8.0F, 8.0F, 8.0F, CubeDeformation.NONE)
 				.texOffs(0, 32).addBox(-4.5F, -4.0F, -5.0F, 9.0F, 2.0F, 10.0F, CubeDeformation.NONE)
-				.texOffs(0, 44).addBox(-4.0F, -7.0F, -4.0F, 8.0F, 3.0F, 8.0F, CubeDeformation.NONE)
 				.texOffs(0, 55).addBox(-3.0F, -9.0F, -3.0F, 6.0F, 2.0F, 6.0F, CubeDeformation.NONE)
 				.texOffs(0, 32).addBox(-1.0F, -9.5F, -1.0F, 2.0F, 1.0F, 2.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(0.0f, 0.0f, -0.25f, -0.0873f, 0.0f, 0.0f));
 		return LayerDefinition.create(mesh, 64, 64);
